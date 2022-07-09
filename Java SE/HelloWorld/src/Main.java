334488668e91f9415c0da1667322886b3eba0f07
Mohammad Mahdi Nemati <adel110@aut.ac.ir>
2022-07-09 13:57:06 +0430

    Hello World and arguments

---
+++ Java SE/HelloWorld/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.printf("Args: %s%n", Arrays.toString(args));
+    }
+}
